37387bec04c4ceac255ac7d926f2843c95403b80
Andrew-Shanan <andrew.shanan@hotmail.com>
2021-11-29 14:02:34 -0800

    moves component

--- movie-rater/src/components/movies.jsx
+++ movie-rater/src/components/movies.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+// in this component 25 moives will be fetched
 const Movies = () => {
+    const [data, setData] = useState({ hits: [] });
+    
+    useEffect(async () => {
+      const result = await axios(
+        'https://imdb-api.com/en/API/Top250Movies/k_tqc3a27f',
+      );
+
+      setData(result.data);
+    });
+    
+
     return ( 
-        <>
-        <h1>All Movies will display here soon...</h1>
-        </>
+        <ul>
+        {data.hits.map(item => (
+          <li key={item.id}>
+            <a href={item.image}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
      );
 }
  
